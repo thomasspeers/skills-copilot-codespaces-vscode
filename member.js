af53db5d250353be79af870b42d916261d3671ec
thomasspeers <113366615+thomasspeers@users.noreply.github.com>
2024-05-06 13:25:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    let member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["HTML", "CSS", "JS"],
+        salary: 25000
+    };
+    return member.skills;
+}
